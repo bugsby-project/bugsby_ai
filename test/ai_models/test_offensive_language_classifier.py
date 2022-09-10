a0f7ac506d4c2865ec7411145e1c454a390365cf
Oskar Picus <oskarpicus@yahoo.com>
2022-09-10 16:20:31 +0300

    Tested Offensive Language Classifier

---
+++ test/ai_models/test_offensive_language_classifier.py
@@ -0,0 +1,14 @@
+import pytest
+
+from artificial_intelligence.ai_models.OffensiveLanguageClassifier import OffensiveLanguageClassifier
+
+
+@pytest.fixture
+def offensive_language_classifier():
+    return OffensiveLanguageClassifier(model_path="resources/aiModels/offensiveLanguageModel.joblib")
+
+
+def test_offensive_language_classifier_predict(offensive_language_classifier):
+    text = "flowers are beautiful"
+    probability = offensive_language_classifier.predict(text)
+    assert float(probability) == probability, "Offensive Language Classifier does not return a float"
